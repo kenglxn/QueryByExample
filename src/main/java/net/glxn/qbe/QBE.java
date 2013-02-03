f2d6d298766870dde3c2589097f2c32cb421d65b
Ken Gullaksen <ken.gullaksen@embriq.no>
2013-02-03 09:59:53 +0100

    updated javadoc

--- src/main/java/net/glxn/qbe/QBE.java
+++ src/main/java/net/glxn/qbe/QBE.java
@@ -20,16 +20,9 @@ public class QBE {
     }
 
     /**
-     * Construct a new {@link QBE} using the supplied entityManager<br/>
-     * <br/>
-     * The {@link QBE} gives the ability to: <br/>
-     * * get a list of entities matching the given criteria {@link net.glxn.qbe.QBEExample#list()}<br/>
-     * {@code new QueryByExample(entityManager).query(UserEntity.class).example(new User("foo")).list()}<br/><br/>
-     * * get a single entity matching the given criteria {@link net.glxn.qbe.QBEExample#item()}<br/>
-     * {@code new QueryByExample(entityManager).query(UserEntity.class).example(new User("foo")).item()}<br/><br/>
-     * *get the resulting typed query without executing it {@link net.glxn.qbe.QBEExample#getQuery()}<br/>
-     * {@code new QueryByExample(entityManager).query(UserEntity.class).example(new User("foo")).getQuery()}<br/><br/>
-     * {@link net.glxn.qbe.QBEExample#getQuery()} can be useful if you need to do paging on the typed query.
+     * Construct a new {@link QBE} using the supplied entityManager
+     *
+     * From here you do {@link QBE#query(Class)}
      *
      * @param entityManager the entityManager to use to create the queries.
      */
@@ -39,6 +32,7 @@ public class QBE {
 
     /**
      * Create a {@link QBEQuery} for the given JPA entity class.
+     * 
      * @param entityClass a JPA entity class. Must be known to the entityManager this {@link QBE} instance was created with
      * @param <E> the entity class type
      * @return a new {@link QBEQuery} for the given entity class
